974e922ca1b46c8b1941cf9090e2017a8f4075bc
Matteo Abis <gmatteo..abis@gmail.com>
2015-03-30 10:38:06 +0200

    remove cut pilatus

--- cut_pilatus.py
+++
@@ -1,8 +0,0 @@
-import h5py
-
-with h5py.File("data/pilatus_time_series.hdf5") as input_file:
-    group = input_file["raw_images"]
-    for i, key in enumerate(group):
-        index = int(key.split("_")[2])
-        if index < 59146:
-            del group[key]
